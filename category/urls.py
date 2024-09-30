303e256ba5abcbd682c2fe7c27c141479c82cffd
shahzodbeeck <shahzodomonboyev1.com>
2024-09-30 18:25:30 +0500

    asdasd

--- category/urls.py
+++ category/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet
+from .views import CategoryViewSet, CategoryServicesViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
-router.register(r'category_services', CategoryViewSet)
+router.register(r'category_services', CategoryServicesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- category/views.py
+++ category/views.py
@@ -1,14 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, CategoryServices
+from .serializers import CategorySerializer, CategoryServicesSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+
 class CategoryServicesViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    queryset = CategoryServices.objects.all()
+    serializer_class = CategoryServicesSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
