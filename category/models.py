6776aa99218957d4bf01592cbb4849c104bb62fe
shahzodbeeck <shahzodomonboyev1.com>
2024-09-30 17:45:09 +0500

    sdsdd

---
+++ category/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    name_en = models.CharField(max_length=100)
+    name_ru = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
